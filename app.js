2c63d299a2a022c9cd05e933629aedcbd83ed836
Mark cabanes <cabanesdev@gmail.com>
2021-09-18 00:10:26 +0200

    Create index.html, app.js and create the canvas configuration

---
+++ app.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+	//Canvas configuration
+	let canvas = $('#canvas')[0];
+	const ctx = canvas.getContext('2d');
+	let width = canvas.width();
+	let height = canvas.height();
+
+	let cellWidth = 15;
+	let direction, food, score, level;
+	let snake_length;
+});
